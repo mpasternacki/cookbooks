c7e10d37256266aec3ef2911c1d32256b128148b
Maciej Pasternacki <maciej@pasternacki.net>
2011-02-17 16:58:50 +0100

    Add s3sync cookbook.

---
+++ s3sync/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:s3sync][:tarball_url] = "http://s3.amazonaws.com/ServEdge_pub/s3sync/s3sync.tar.gz"
+default[:s3sync][:tarball_sha256] = "ee58d6f9e3cbd108ccc1680eaee63baac6c6d88e8b4de82204aa81ea5f3d86a4"
---
+++ s3sync/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Maciej Pasternacki"
+maintainer_email "maciej@pasternacki.net"
+license          "All rights reserved"
+description      "Installs/Configures s3sync"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+supports "debian"
+supports "ubuntu"
