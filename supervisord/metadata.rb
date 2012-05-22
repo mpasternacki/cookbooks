096001780c7b00cc8b5e67488bfd1e6d96af3a56
Dan Crosta <dcrosta@late.am>
2012-05-21 23:56:00 -0400

    update metadata and README
    * bumped version to 0.1.0 (follows semver)
    * added myself and Maciej to authors in README

--- supervisord/metadata.rb
+++ supervisord/metadata.rb
@@ -3,6 +3,7 @@ maintainer_email "maciej@pasternacki.net"
 license          "WTFPL"
 description      "Installs/Configures supervisord"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.1.0"
 
 supports "ubuntu"
+supports "debian", ">= 6.0"
