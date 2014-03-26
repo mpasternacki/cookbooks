e4aeb8451bb79296339116f2a5501f684a2092ad
Alex Hewson <alex@mbird.biz>
2014-03-26 14:29:50 +0000

    Sleep a little after restarting.  This is not especially pretty, but without it the control socket may not have appeared when we try to control supervisord_program's later in the run

--- supervisord/recipes/default.rb
+++ supervisord/recipes/default.rb
@@ -1,6 +1,8 @@
 package "supervisor"            # FIXME: work with custom debian package
 
 service "supervisor" do
-  reload_command "supervisorctl reload"
+  # Sleep a few seconds to ensure the xmlrpc interface is available again.
+  # Without this the restarting of supervisor_program's will randomly fail.
+  reload_command "supervisorctl reload && sleep 5"
   action [ :enable, :start ]
 end
